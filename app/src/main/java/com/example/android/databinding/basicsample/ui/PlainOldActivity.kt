166ae97a66206db4f67e958531152266d0469f9f
Tori Lew <torirlew@gmail.com>
2023-02-22 13:55:19 -0800

    Creates and uses binding adapter attributes

--- app/src/main/java/com/example/android/databinding/basicsample/ui/PlainOldActivity.kt
+++ app/src/main/java/com/example/android/databinding/basicsample/ui/PlainOldActivity.kt
@@ -52,29 +52,4 @@ class PlainOldActivity : AppCompatActivity() {
 
         binding.viewmodel = viewModel
     }
-
-
-    private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
-        return when (popularity) {
-            Popularity.NORMAL -> context.theme.obtainStyledAttributes(
-                intArrayOf(android.R.attr.colorForeground)
-            ).getColor(0, 0x000000)
-            Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
-            Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
-        }
-    }
-
-    private fun getDrawablePopularity(popularity: Popularity, context: Context): Drawable? {
-        return when (popularity) {
-            Popularity.NORMAL -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_person_black_96dp)
-            }
-            Popularity.POPULAR -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-            }
-            Popularity.STAR -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-            }
-        }
-    }
 }
